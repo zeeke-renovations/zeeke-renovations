475b4d787ff3f04c1c1b6bf28b4e5be055c58bc1
zeeke <155785890+zeeke-renovations@users.noreply.github.com>
2026-04-05 20:45:20 -0500

    fix-3

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import { Home } from "./pages/home";
+import { Services } from "./pages/services";
+import { Gallery } from "./pages/gallery";
+import { Contact } from "./pages/contact";
+import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
+
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
+      <Footer />
+      <Header />
     </BrowserRouter>
   );
 }
